b382179eacc92a0943039b97c7feace915d30474
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2021-07-18 21:03:40 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+![our_cool_image](logo.png)
+# Texaas
+
+## A PDF Templating Microservice powered by LaTeX
+
+More Coming #100daysofcode
---
+++ texaas/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/health')
+def health():
+	return jsonify(success=True)
+
+
+if __name__ == '__main__':
+	app.run()
